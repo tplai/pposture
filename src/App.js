6dc01f0a6828bab8ef030cf1188df334d39db0a8
tplai <thomas.lai920@gmail.com>
2020-10-31 21:12:01 -0400

    Uploaded images now have posenet run on it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import imgsrc from './images/gamer2.png';
-import logo from './logo.svg';
-import React, { useEffect, useRef, } from 'react';
+import logo from './logo.png';
+import React, { useEffect, useRef, useState, } from 'react';
+import ImageUploader from "react-images-upload";
 import Navbar from 'react-bootstrap/Navbar';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,9 +9,6 @@ const posenet = require('@tensorflow-models/posenet');
 require('@tensorflow/tfjs-backend-webgl');
 
 const confidence = 0.8;
-
-const imgHeight = 417;
-const imgWidth = 357;
 
 const rightside = [2, 4, 6, 8, 10, 12, 14, 16];
 const leftside = [1, 3, 5, 7, 9, 11, 13, 15];
@@ -42,67 +39,89 @@ export default function App() {
   const imgref = useRef();
   const canvasref = useRef();
 
+  const [imgWidth, setImgWidth] = useState(-1);
+  const [imgHeight, setImgHeight] = useState(-1);
+  const [picture, setPicture] = useState();
+
+  const onDrop = picture => {
+    // console.log(picture);
+    // setPictures(picture);
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      // console.log(e.target);
+      setPicture(e.target.result);
+      // console.log(e.target);
+    }
+    reader.readAsDataURL(picture[0]);
+  };
+
   useEffect(() => {
-    const pose = estimatePoseOnImage(imgref.current);
-    // console.log(pose);
-    pose.then((res) => {
-      console.log(res);
-      // console.log(canvasref.current);
-      let ctx = canvasref.current.getContext('2d');
-      let scale = 1; // get the min scale to fit
-      let x = (ctx.canvas.width - (imgWidth * scale) ) / 2; // centre x
-      let y = (ctx.canvas.height - (imgHeight * scale) ) / 2; // centre y
+    if (picture) {
+      setImgWidth(imgref.current.naturalWidth);
+      setImgHeight(imgref.current.naturalHeight);
+      // console.log(pictures);
+      const pose = estimatePoseOnImage(imgref.current);
+      // console.log(pose);
+      pose.then((res) => {
+        console.log(res);
+        console.log(canvasref.current);
+        console.log(imgref.current);
 
-      ctx.drawImage(imgref.current, x, y, imgWidth * scale, imgHeight * scale); // draw scaled img onto the canvas.
+        let ctx = canvasref.current.getContext('2d');
+        let scale = 1; // get the min scale to fit
+        let x = (ctx.canvas.width - (imgWidth * scale) ) / 2; // centre x
+        let y = (ctx.canvas.height - (imgHeight * scale) ) / 2; // centre y
+        // ctx.drawImage(imgref.current)
+        ctx.drawImage(imgref.current, x, y, imgWidth * scale, imgHeight * scale); // draw scaled img onto the canvas.
 
-      let rightscore = 0;
-      let leftscore = 0;
+        let rightscore = 0;
+        let leftscore = 0;
 
-      // loop through keypoints
-      for (let i = 0; i < res.keypoints.length; i++) {
-        // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-        let keypoint = res.keypoints[i];
-        // draw lines
-        // Only draw an ellipse is the pose probability is bigger than confidence
-        if (keypoint.score > confidence) {
-          // point
-          ctx.fillStyle = "#00FFFF";
-          ctx.beginPath();
-          ctx.arc(keypoint.position.x, keypoint.position.y, 4, 0, 2 * Math.PI, true);
-          ctx.fill();
+        // loop through keypoints
+        for (let i = 0; i < res.keypoints.length; i++) {
+          // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+          let keypoint = res.keypoints[i];
+          // draw lines
+          // Only draw an ellipse is the pose probability is bigger than confidence
+          if (keypoint.score > confidence) {
+            // point
+            ctx.fillStyle = "#00FFFF";
+            ctx.beginPath();
+            ctx.arc(keypoint.position.x, keypoint.position.y, 4, 0, 2 * Math.PI, true);
+            ctx.fill();
 
-          // line
-          if (i in keypointlines) {
-            for (let con in keypointlines[i]) {
-              // console.log(res.keypoints[keypointlines[i][con]]);
-              // console.log(i);
-              if (res.keypoints[keypointlines[i][con]].score > confidence) {
-                // console.log("draw line from " + i + " to " + keypointlines[i][con]);
-                ctx.beginPath();
-                ctx.moveTo(keypoint.position.x, keypoint.position.y);
-                ctx.lineTo(res.keypoints[keypointlines[i][con]].position.x, res.keypoints[keypointlines[i][con]].position.y);
+            // line
+            if (i in keypointlines) {
+              for (let con in keypointlines[i]) {
+                if (res.keypoints[keypointlines[i][con]].score > confidence) {
+                  // console.log("draw line from " + i + " to " + keypointlines[i][con]);
+                  ctx.beginPath();
+                  ctx.moveTo(keypoint.position.x, keypoint.position.y);
+                  ctx.lineTo(res.keypoints[keypointlines[i][con]].position.x, res.keypoints[keypointlines[i][con]].position.y);
 
-                ctx.lineWidth = 3;
-                ctx.strokeStyle = "#00FFFF";
-                ctx.stroke();
+                  ctx.lineWidth = 3;
+                  ctx.strokeStyle = "#00FFFF";
+                  ctx.stroke();
+                }
               }
             }
           }
-        }
 
-        // compute average left or right side
-        if (rightside.includes(i)) {
-          rightscore += keypoint.score;
+          // compute average left or right side
+          if (rightside.includes(i)) {
+            rightscore += keypoint.score;
+          }
+          else if (leftside.includes(i)) {
+            leftscore += keypoint.score;
+          }
         }
-        else if (leftside.includes(i)) {
-          leftscore += keypoint.score;
-        }
-      }
-      // averages
-      console.log(`Right side average ${rightscore / rightside.length}`);
-      console.log(`Left side average ${leftscore / leftside.length}`);
-    })
-  });
+        // averages
+        console.log(`Right side average ${rightscore / rightside.length}`);
+        console.log(`Left side average ${leftscore / leftside.length}`);
+      })
+
+    }
+  }, [picture, imgHeight, imgWidth]);
 
   return (
     <div className="App">
@@ -119,9 +138,18 @@ export default function App() {
         </Navbar.Brand>
       </Navbar>
       <div className="body">
-        <img src={imgsrc} style={{height: imgHeight, width: imgWidth, display: 'none'}} ref={imgref} />
-        <canvas ref={canvasref} width={imgWidth} height={imgHeight}/>
-      </div>
+        <div className="upload">
+          <ImageUploader
+            withIcon={true}
+            singleImage={true}
+            onChange={onDrop}
+            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+            maxFileSize={5242880}
+          />
+        </div>
+        <img src={picture} alt="upload" style={{display: 'none'}} ref={imgref} />
+        <canvas width={imgWidth} height={imgHeight} ref={canvasref} />
+    </div>
     </div>
   );
 }
